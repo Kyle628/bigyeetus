2c3919324ee3bcf4a856fc098612035839327ff6
Kyle O'Connor <kyjoconn@gmail.com>
2020-09-16 07:05:50 -0700

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="https://media1.tenor.com/images/9fd443a1453bb9a797fb5bb26f76e77f/tenor.gif?itemid=18099467"></img>
+    </div>
+  );
+}
+
+export default App;
